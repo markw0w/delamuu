d1bec6b143cd9a4ece6641b6a3789e77911a3148
Estanislao Salinas <estanisprevite@hotmail.com>
2025-03-26 00:54:40 -0500

    feat: update database

--- data/delamuu_db.sql
+++ data/delamuu_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-03-2025 a las 01:44:53
+-- Tiempo de generación: 26-03-2025 a las 06:54:14
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -48,7 +48,7 @@ CREATE TABLE `delivery` (
 --
 
 INSERT INTO `delivery` (`delivery_price_id`, `price`) VALUES
-(1, 20000);
+(1, 2000);
 
 -- --------------------------------------------------------
 
@@ -66,10 +66,10 @@ CREATE TABLE `envases` (
 --
 
 INSERT INTO `envases` (`id`, `nombre`) VALUES
-(2, '1/2 kg'),
-(3, '3/4 kg'),
-(4, '1 kg'),
-(11, '1/4 kg');
+(12, '1/4 kg'),
+(13, '1/2 kg'),
+(14, '3/4 kg'),
+(15, '1 kg');
 
 -- --------------------------------------------------------
 
@@ -89,23 +89,22 @@ CREATE TABLE `envases_productos` (
 --
 
 INSERT INTO `envases_productos` (`id`, `envase_id`, `producto_id`, `precio`) VALUES
-(6, 2, 2, 1000.00),
-(7, 3, 2, 32000.00),
-(8, 4, 2, 42000.00),
-(10, 2, 3, 25000.00),
-(11, 3, 3, 35000.00),
-(12, 4, 3, 45000.00),
-(14, 2, 4, 28000.00),
-(15, 3, 4, 38000.00),
-(16, 4, 4, 48000.00),
-(17, 11, 2, 0.00),
-(18, 11, 3, 0.00),
-(19, 11, 4, 25000.00),
-(20, 11, 5, 0.00),
-(21, 2, 6, 0.00),
-(22, 3, 6, 0.00),
-(23, 4, 6, 0.00),
-(24, 11, 6, 10000.00);
+(32, 12, 2, 5900.00),
+(33, 12, 3, 7500.00),
+(34, 12, 4, 8500.00),
+(35, 12, 5, 6500.00),
+(36, 13, 2, 10900.00),
+(37, 13, 3, 12900.00),
+(38, 13, 4, 15500.00),
+(39, 13, 5, 11900.00),
+(40, 14, 2, 14900.00),
+(41, 14, 3, 0.00),
+(42, 14, 4, 22900.00),
+(43, 14, 5, 0.00),
+(44, 15, 2, 18500.00),
+(45, 15, 3, 17500.00),
+(46, 15, 4, 28900.00),
+(47, 15, 5, 15900.00);
 
 -- --------------------------------------------------------
 
@@ -123,18 +122,19 @@ CREATE TABLE `frutas` (
 --
 
 INSERT INTO `frutas` (`id`, `nombre`) VALUES
-(1, 'Ananá'),
-(2, 'Arándanos'),
-(3, 'Cerezas'),
-(4, 'Durazno'),
-(13, 'Ensalada de frutas'),
-(5, 'Frutilla'),
-(6, 'Frutos rojos'),
-(7, 'Higo en almibar'),
-(9, 'Melón'),
-(10, 'Sandía'),
-(11, 'Uva'),
-(12, 'Zapallo en almíbar');
+(15, 'Ananá'),
+(17, 'Arándanos'),
+(18, 'Cerezas'),
+(19, 'Durazno'),
+(28, 'Ensalada de frutas'),
+(20, 'Frutilla'),
+(21, 'Frutos rojos'),
+(22, 'Higos en almibar'),
+(23, 'Kiwi'),
+(24, 'Melón'),
+(25, 'Sandía'),
+(26, 'Uva'),
+(27, 'Zapallo en almíbar');
 
 -- --------------------------------------------------------
 
@@ -159,7 +159,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `nombre_cliente`, `direccion`, `precio`, `fecha`, `detalles`, `total`, `forma_pago`, `forma_retiro`) VALUES
-(16, 'Estanis', '123', 0.00, '2025-03-24 21:47:12', '[{\"gramaje\":\"1/2 kg\",\"toppings\":[\"Alfajor\",\"Anillitos\"],\"salsas\":[],\"frutas\":[],\"precio\":20000}]', 40000.00, 'Pago contraentrega', 'Entregar en domicilio');
+(22, 'Estanis', 'Av123', 0.00, '2025-03-26 05:40:23', '[{\"product\":\"Yogur\",\"gramaje\":\"1/4 kg\",\"toppings\":[],\"salsas\":[],\"frutas\":[\"Zapallo en almíbar\",\"Uva\",\"Sandía\"],\"precio\":6500},{\"product\":\"Helado\",\"gramaje\":\"1/2 kg\",\"sabores\":[\"Almendrado\",\"Americana\",\"Ananá\"],\"precio\":10900},{\"product\":\"Azai\",\"gramaje\":\"3/4 kg\",\"toppings\":[\"Alfajor\",\"Almendras\",\"Almohaditas\",\"Anillitos\"],\"salsas\":[],\"frutas\":[],\"precio\":22900},{\"product\":\"Candy\",\"gramaje\":\"1 kg\",\"toppings\":[\"Pasas de uva con choco\",\"Ópera\",\"Moritas\"],\"salsas\":[\"Frutos del bosque\",\"Frutilla\"],\"frutas\":[],\"precio\":17500}]', 59800.00, 'Pago contraentrega', 'Entregar en domicilio');
 
 -- --------------------------------------------------------
 
@@ -180,8 +180,69 @@ INSERT INTO `productos` (`id`, `nombre`) VALUES
 (2, 'Helado'),
 (3, 'Candy'),
 (4, 'Azai'),
-(5, 'Yogur'),
-(6, 'Milanesa');
+(5, 'Yogur');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sabores`
+--
+
+CREATE TABLE `sabores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sabores`
+--
+
+INSERT INTO `sabores` (`id`, `nombre`) VALUES
+(5, 'Almendrado'),
+(6, 'Americana'),
+(7, 'Ananá'),
+(8, 'Banana Split'),
+(9, 'Bis-Choc'),
+(10, 'Chocolate'),
+(11, 'Chocolate al rhun'),
+(12, 'Chocolate amargo'),
+(13, 'Chocolate blanco'),
+(14, 'Chocolate con almendras'),
+(15, 'Chocolate selva negra'),
+(16, 'Chocolate suizo'),
+(17, 'Coco con dulce de leche'),
+(18, 'Crema del cielo'),
+(19, 'Crema oreo'),
+(20, 'Cereza'),
+(21, 'Dulce de leche'),
+(22, 'Dulce de leche con nuez'),
+(23, 'Dulce de leche granizado'),
+(24, 'Durazno'),
+(25, 'Flan'),
+(26, 'Frutilla al agua'),
+(27, 'Frutilla a la crema'),
+(28, 'Frutos del bosque'),
+(29, 'Granizado'),
+(30, 'Kiwi'),
+(31, 'Lemon Pie'),
+(32, 'Limón'),
+(33, 'Limón granizado'),
+(34, 'Limón mediterráneo'),
+(35, 'Mantecol'),
+(36, 'Mascarpone'),
+(37, 'Menta granizada'),
+(38, 'Mousse de chocolate'),
+(39, 'Nevado'),
+(40, 'Naranja'),
+(41, 'Panna cota'),
+(42, 'Pistacho'),
+(43, 'Rocher'),
+(44, 'Súper Dulce de leche'),
+(45, 'Súper sambayon'),
+(46, 'Tiramisú'),
+(47, 'Tramontana'),
+(48, 'Vainilla'),
+(49, 'Yogurt');
 
 -- --------------------------------------------------------
 
@@ -199,27 +260,27 @@ CREATE TABLE `salsas` (
 --
 
 INSERT INTO `salsas` (`id`, `nombre`) VALUES
-(12, 'Bon o bon'),
-(13, 'Brownie'),
-(14, 'Choco & Naranja'),
-(15, 'Chocolate blanco'),
-(2, 'Chocotorta'),
-(3, 'Cream de coco'),
-(11, 'Crema oreo'),
-(4, 'Dulce de leche'),
-(5, 'Dulce de leche granizado'),
-(6, 'Frutilla'),
-(7, 'Frutos del bosque'),
-(16, 'Gran fondente'),
-(17, 'Kinder'),
-(8, 'Leche condensada'),
-(19, 'Maní crunch'),
-(18, 'Mantequilla de maní'),
-(10, 'Maracuyá'),
-(20, 'Mentita felfort'),
-(21, 'Miel'),
-(9, 'Nutella'),
-(22, 'Test');
+(23, 'Banana Split'),
+(35, 'Bon o Bon (Caliente)'),
+(36, 'Brownie (Caliente)'),
+(37, 'Choco & Naranja (Caliente)'),
+(38, 'Chocolate blanco (Caliente)'),
+(25, 'Chocotorta'),
+(26, 'Crema de coco'),
+(34, 'Crema oreo'),
+(27, 'Dulce de leche'),
+(28, 'Dulce de leche granizado'),
+(29, 'Frutilla'),
+(30, 'Frutos del bosque'),
+(39, 'Gran fondente (Caliente)'),
+(40, 'Kinder (Caliente)'),
+(31, 'Leche condensada'),
+(42, 'Maní crunch (Caliente)'),
+(41, 'Mantequilla de maní (Caliente)'),
+(33, 'Maracuyá'),
+(43, 'Mentita fort (Caliente)'),
+(44, 'Miel (Caliente)'),
+(32, 'Nutella');
 
 -- --------------------------------------------------------
 
@@ -231,6 +292,42 @@ CREATE TABLE `toppings` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `toppings`
+--
+
+INSERT INTO `toppings` (`id`, `nombre`) VALUES
+(38, 'Alfajor'),
+(39, 'Almendras'),
+(40, 'Almohaditas'),
+(41, 'Anillitos'),
+(42, 'Bananitas'),
+(43, 'Bon o Bon'),
+(44, 'Cereal con chocolate'),
+(45, 'Chips con chocolate'),
+(46, 'Chocolinas'),
+(47, 'Coco rallado'),
+(48, 'Frutos secos'),
+(49, 'Gomitas'),
+(51, 'Granas multicolor'),
+(50, 'Granola'),
+(52, 'Leche en Polvo Nido'),
+(53, 'Maní con chocolate'),
+(54, 'Maní crocante'),
+(55, 'Mantecol'),
+(56, 'Marroc'),
+(57, 'Merenguitos'),
+(59, 'Micro cereales de color'),
+(58, 'Mini galletitas con choco'),
+(60, 'Moritas'),
+(61, 'Ópera'),
+(62, 'Oreo'),
+(63, 'Pasas de uva con choco'),
+(64, 'Pepitos'),
+(65, 'Quinoa con chocolate'),
+(66, 'Rocklets'),
+(67, 'Zucarita');
 
 -- --------------------------------------------------------
 
@@ -295,6 +392,12 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `sabores`
+--
+ALTER TABLE `sabores`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `salsas`
 --
 ALTER TABLE `salsas`
@@ -328,43 +431,49 @@ ALTER TABLE `delivery`
 -- AUTO_INCREMENT de la tabla `envases`
 --
 ALTER TABLE `envases`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `envases_productos`
 --
 ALTER TABLE `envases_productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de la tabla `frutas`
 --
 ALTER TABLE `frutas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `sabores`
+--
+ALTER TABLE `sabores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de la tabla `salsas`
 --
 ALTER TABLE `salsas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de la tabla `toppings`
 --
 ALTER TABLE `toppings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
