eb295a3020d6487b26cf9747f372736d844eda03
Estanislao Salinas <estanisprevite@hotmail.com>
2025-03-31 04:00:56 -0500

    fix: app.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/main.css" // Todo SaSS llega a main.scss quien convierte a main.css
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
