440025c99aca759f57a0efe2d391cf3506ff7da9
Estanislao Salinas <estanisprevite@hotmail.com>
2025-03-31 03:38:41 -0500

    fix: user fix

--- src/db/cnn.js
+++ src/db/cnn.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 const MYSQL_DATABASE = 'delamuu_db';
-const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_USER = 'delamuu';
+const MYSQL_PASSWORD = 'Delamuu2025#ABC123';
 const MYSQL_HOST = '127.0.0.1';
 const MYSQL_PORT = 3306;
 
