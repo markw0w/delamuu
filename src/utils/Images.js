10cf392238f46acdc6c9b180863ec6abf6fe7c96
Estanislao Salinas <estanisprevite@hotmail.com>
2025-03-31 12:47:41 -0500

    update: .png and .jpg to .webp

--- src/utils/Images.js
+++ src/utils/Images.js
@@ -1,106 +1,105 @@
 // Importamos
 // Imports: Local
-import Local1 from '../assets/store/local.jpg' 
-import Local2 from '../assets/store/local2.jpg' 
-import Local3 from '../assets/store/local3.jpg' 
-import Local4 from '../assets/store/local-detail.jpg' 
-import Local5 from '../assets/store/local-outside.jpg' 
+import Local1 from "../assets/store/local.webp";
+import Local2 from "../assets/store/local2.webp";
+import Local3 from "../assets/store/local3.webp";
+import Local4 from "../assets/store/local-detail.webp";
+import Local5 from "../assets/store/local-outside.webp";
 
 // Imports: LOGO & Delamuu
-import LogoShortLight from '../assets/delamuu-logos/logo-short-light.png';
-import LogoShortDark from '../assets/delamuu-logos/logo-short-dark.png';
-import LogoLongTransparent from '../assets/delamuu-logos/logo-long-transparent.png';
+import LogoShortLight from "../assets/delamuu-logos/logo-short-light.webp";
+import LogoShortDark from "../assets/delamuu-logos/logo-short-dark.webp";
+import LogoLongTransparent from "../assets/delamuu-logos/logo-long-transparent.webp";
 
 // Imports: Productos
 // Yogur
-import Yogur1 from '../assets/products/yogur_1.jpg';
-import Yogur2 from '../assets/products/yogur_2.jpg';
-import Yogur3 from '../assets/products/yogur_3.jpg';
-import Yogur4 from '../assets/products/yogur_4.jpg';
-import Yogur5 from '../assets/products/yogur_5.jpg';
-import Yogur6 from '../assets/products/yogur_6.jpg';
-import YogurStrawberries from '../assets/products/yogur-strawberries.jpg';
-import HandYogur from '../assets/products/hand-yogur.jpg';
-import EnsaladaFrutas from '../assets/products/ensalada-frutas.jpg';
+import Yogur1 from "../assets/products/yogur_1.webp";
+import Yogur2 from "../assets/products/yogur_2.webp";
+import Yogur3 from "../assets/products/yogur_3.webp";
+import Yogur4 from "../assets/products/yogur_4.webp";
+import Yogur5 from "../assets/products/yogur_5.webp";
+import Yogur6 from "../assets/products/yogur_6.webp";
+import YogurStrawberries from "../assets/products/yogur-strawberries.webp";
+import HandYogur from "../assets/products/hand-yogur.webp";
+import EnsaladaFrutas from "../assets/products/ensalada-frutas.webp";
 
 // Helado
-import Helado1 from '../assets/products/helado_1.jpg';
-import Helado2 from '../assets/products/helado_2.jpg';
+import Helado1 from "../assets/products/helado_1.webp";
+import Helado2 from "../assets/products/helado_2.webp";
 
 // Candy
-import Candy1 from '../assets/products/candy_1.jpg';
-import Candy2 from '../assets/products/candy_2.jpg';
+import Candy1 from "../assets/products/candy_1.webp";
+import Candy2 from "../assets/products/candy_2.webp";
 
 // Acai
-import Acai1 from '../assets/products/acaí_1.jpg';
-import Acai2 from '../assets/products/acaí_2.jpg';
-import Acai3 from '../assets/products/acaí_3.jpg';
-import Acai4 from '../assets/products/acaí_4.jpg';
-import Acai5 from '../assets/products/acaí_5.jpg';
-import Acai6 from '../assets/products/acaí_6.jpg';
-import Acai7 from '../assets/products/acaí_7.jpg';
-import AcaiBowl from '../assets/products/acaí_bowl.jpg';
+import Acai1 from "../assets/products/acaí_1.webp";
+import Acai2 from "../assets/products/acaí_2.webp";
+import Acai3 from "../assets/products/acaí_3.webp";
+import Acai4 from "../assets/products/acaí_4.webp";
+import Acai5 from "../assets/products/acaí_5.webp";
+import Acai6 from "../assets/products/acaí_6.webp";
+import Acai7 from "../assets/products/acaí_7.webp";
+import AcaiBowl from "../assets/products/acaí_bowl.webp";
 
 // Imports: Efectos fondo
-import ArandanosEffect from '../assets/others/strawberries.png';
-import StrawberriesEffect from '../assets/others/frutillas.png';
-import YogurEffect from '../assets/others/helado-principal.png';
-import AzaiEffect from '../assets/others/azai.png';
+import ArandanosEffect from "../assets/others/strawberries.webp";
+import StrawberriesEffect from "../assets/others/frutillas.webp";
+import YogurEffect from "../assets/others/helado-principal.webp";
+import AzaiEffect from "../assets/others/azai.webp";
 
 // A exportar
 // Logos
 export const logos = {
-    shortLight: LogoShortLight,
-    shortDark: LogoShortDark,
-    logoLogTrans: LogoLongTransparent,
-}
+  shortLight: LogoShortLight,
+  shortDark: LogoShortDark,
+  logoLogTrans: LogoLongTransparent,
+};
 
 // Productos
 export const products = {
-    // Yogur
-    yogur1: Yogur1,
-    yogur2: Yogur2,
-    yogur3: Yogur3,
-    yogur4: Yogur4,
-    yogur5: Yogur5,
-    yogur6: Yogur6,
-    yogurStrawberries: YogurStrawberries,
-    handYogur: HandYogur,
-    ensaladaFrutas: EnsaladaFrutas,
+  // Yogur
+  yogur1: Yogur1,
+  yogur2: Yogur2,
+  yogur3: Yogur3,
+  yogur4: Yogur4,
+  yogur5: Yogur5,
+  yogur6: Yogur6,
+  yogurStrawberries: YogurStrawberries,
+  handYogur: HandYogur,
+  ensaladaFrutas: EnsaladaFrutas,
 
-    // Helado
-    Helado1: Helado1,
-    Helado2: Helado2,
+  // Helado
+  Helado1: Helado1,
+  Helado2: Helado2,
 
-    // Candy
-    Candy1: Candy1,
-    Candy2: Candy2,
+  // Candy
+  Candy1: Candy1,
+  Candy2: Candy2,
 
-    // Acai
-    acai1: Acai1,
-    acai2: Acai2,
-    acai3: Acai3,
-    acai4: Acai4,
-    acai5: Acai5,
-    acai6: Acai6,
-    acai7: Acai7,
-    acaiBowl: AcaiBowl,
-}
+  // Acai
+  acai1: Acai1,
+  acai2: Acai2,
+  acai3: Acai3,
+  acai4: Acai4,
+  acai5: Acai5,
+  acai6: Acai6,
+  acai7: Acai7,
+  acaiBowl: AcaiBowl,
+};
 
 // Efectos fondo
 export const effects = {
-    arandanos: ArandanosEffect,
-    strawberries: StrawberriesEffect,
-    yogur: YogurEffect,
-    azai: AzaiEffect,
-}
+  arandanos: ArandanosEffect,
+  strawberries: StrawberriesEffect,
+  yogur: YogurEffect,
+  azai: AzaiEffect,
+};
 
 // Local
 export const local = {
-    local1: Local1,
-    local2: Local2,
-    local3: Local3,
-    local4: Local4,
-    local5: Local5,
-}
-
+  local1: Local1,
+  local2: Local2,
+  local3: Local3,
+  local4: Local4,
+  local5: Local5,
+};
